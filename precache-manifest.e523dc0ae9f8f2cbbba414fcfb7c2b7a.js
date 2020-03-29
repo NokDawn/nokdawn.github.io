de5e1ab625c19b0f0b467c9c421e20f5258a36d3
NokDawn <nookdawn@gmail.com>
2020-03-29 13:09:53 +0200

    improvements

---
+++ precache-manifest.e523dc0ae9f8f2cbbba414fcfb7c2b7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32daa77123aaf6663ab8a3f296c910a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adecad62c0794281470c",
+    "url": "/static/css/main.27db7109.chunk.css"
+  },
+  {
+    "revision": "e203f9e1897d9e50493c",
+    "url": "/static/js/2.a23677d3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a23677d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "adecad62c0794281470c",
+    "url": "/static/js/main.aaeb262f.chunk.js"
+  },
+  {
+    "revision": "36180433ff2c3de56d8c",
+    "url": "/static/js/runtime-main.50a66773.js"
+  },
+  {
+    "revision": "75b5e167fe69276eb5fcbc21f06ac102",
+    "url": "/static/media/1.75b5e167.png"
+  },
+  {
+    "revision": "a38e6a55e1d678793728816f133c37c7",
+    "url": "/static/media/2.a38e6a55.png"
+  },
+  {
+    "revision": "d921fdabbf6c619a99577faf9c629c39",
+    "url": "/static/media/3.d921fdab.png"
+  },
+  {
+    "revision": "daa8a86d1e2b19d19ffab1947be9fc65",
+    "url": "/static/media/logo.daa8a86d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8deb84f0fcb0dadab475048d8baedbfc.js"
+  "/precache-manifest.e523dc0ae9f8f2cbbba414fcfb7c2b7a.js"
 );
 
 self.addEventListener('message', (event) => {
